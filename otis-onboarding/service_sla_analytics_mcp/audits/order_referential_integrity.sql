c535cadba4d0abb4aca3f513ca2e10f1f87204c8
Deepak Jaiswal <deepakjaiswal@TMDCIN138.local>
2026-08-07 11:52:54 +0530

    Update: the service sla analytics dq and audits according to new model logic changes

--- otis-onboarding/service_sla_analytics_mcp/audits/order_referential_integrity.sql
+++ otis-onboarding/service_sla_analytics_mcp/audits/order_referential_integrity.sql
@@ -1,6 +1,6 @@
 -- Source: design spec > Section 15 Custom Audit Files (orphaned_order_contracts)
 AUDIT (name assert_orphaned_order_contracts);
-SELECT oo.ORDER_KEY
+SELECT oo.WORK_ORDER_ID
 FROM analytics.gold_open_order_facts AS oo
 LEFT JOIN "MDM_NAA_PROD"."MDM_OUTBOUND"."GLOBAL_CONTRACT_MASTER" AS ct
   ON oo.CONTRACT_ID = ct.CONTRACT_ID
